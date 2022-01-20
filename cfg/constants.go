1db9b01aba5ba4fbd6cb92b1d2844347d6cfc204
niels1286 <niels@nuls.io>
2022-01-21 00:51:01 +0800

    add nerve beta macOS version

--- cfg/constants.go
+++ cfg/constants.go
@@ -3,12 +3,12 @@
 // @Author  Niels  2020/9/25
 package cfg
 
-//const ApiUrl = "http://beta.api.nerve.network/jsonrpc/"
-//const PsUrl = "http://beta.public.nerve.network/"
-//const MainChainId = uint16(5)
-//const MainAssetsId = uint16(1)
-//const AddressPrefix = "TNVT"
-//const BlackHoleAddress = "TNVTdTSPGwjgRMtHqjmg8yKeMLnpBpVN5ZuuY"
+const ApiUrl = "http://beta.api.nerve.network/jsonrpc/"
+const PsUrl = "http://beta.public.nerve.network/"
+const MainChainId = uint16(5)
+const MainAssetsId = uint16(1)
+const AddressPrefix = "TNVT"
+const BlackHoleAddress = "TNVTdTSPGwjgRMtHqjmg8yKeMLnpBpVN5ZuuY"
 
 //var AssetsMap = map[string]int{"2-1": 8, "5-1": 8, "5-2": 18, "5-6": 6, "5-7": 6, "5-8": 18, "5-9": 18}
 
@@ -26,12 +26,12 @@ package cfg
 // const AddressPrefix = "NULS"
 // const BlackHoleAddress = "NULSd6HgWSU1iR6BfNoQi85mAMT52JMFzpnok"
 
-const ApiUrl = "http://beta.api.nuls.io/jsonrpc/"
-const PsUrl = "http://beta.public1.nuls.io/"
-const MainChainId = uint16(2)
-const MainAssetsId = uint16(1)
-const AddressPrefix = "tNULS"
-const BlackHoleAddress = "tNULSeBaMhZnRteniCy3UZqPjTbnWKBPHX1a5d"
+// const ApiUrl = "http://beta.api.nuls.io/jsonrpc/"
+// const PsUrl = "http://beta.public1.nuls.io/"
+// const MainChainId = uint16(2)
+// const MainAssetsId = uint16(1)
+// const AddressPrefix = "tNULS"
+// const BlackHoleAddress = "tNULSeBaMhZnRteniCy3UZqPjTbnWKBPHX1a5d"
 
 const POCLockValue = 18446744073709551615
 
