10358d5ffdff8db13597f59b524f961a1cbe959a
niels1286 <niels@nuls.io>
2022-03-21 17:07:17 +0800

    支持NULS_NVT_LP资产

--- cfg/constants.go
+++ cfg/constants.go
@@ -3,21 +3,21 @@
 // @Author  Niels  2020/9/25
 package cfg
 
-const ApiUrl = "http://beta.api.nerve.network/jsonrpc/"
-const PsUrl = "http://beta.public.nerve.network/"
-const MainChainId = uint16(5)
-const MainAssetsId = uint16(1)
-const AddressPrefix = "TNVT"
-const BlackHoleAddress = "TNVTdTSPGwjgRMtHqjmg8yKeMLnpBpVN5ZuuY"
+// const ApiUrl = "http://beta.api.nerve.network/jsonrpc/"
+// const PsUrl = "http://beta.public.nerve.network/"
+// const MainChainId = uint16(5)
+// const MainAssetsId = uint16(1)
+// const AddressPrefix = "TNVT"
+// const BlackHoleAddress = "TNVTdTSPGwjgRMtHqjmg8yKeMLnpBpVN5ZuuY"
 
 //var AssetsMap = map[string]int{"2-1": 8, "5-1": 8, "5-2": 18, "5-6": 6, "5-7": 6, "5-8": 18, "5-9": 18}
 
-// const ApiUrl = "https://api.nerve.network/jsonrpc/"
-// const PsUrl = "https://public.nerve.network/"
-// const MainChainId = uint16(9)
-// const MainAssetsId = uint16(1)
-// const AddressPrefix = "NERVE"
-// const BlackHoleAddress = "NERVEepb63T1M8JgQ26jwZpZXYL8ZMLdUAK31L"
+const ApiUrl = "https://api.nerve.network/jsonrpc/"
+const PsUrl = "https://public.nerve.network/"
+const MainChainId = uint16(9)
+const MainAssetsId = uint16(1)
+const AddressPrefix = "NERVE"
+const BlackHoleAddress = "NERVEepb63T1M8JgQ26jwZpZXYL8ZMLdUAK31L"
 
 // const ApiUrl = "https://api.nuls.io/jsonrpc/"
 // const PsUrl = "https://public1.nuls.io/"
@@ -35,4 +35,4 @@ const BlackHoleAddress = "TNVTdTSPGwjgRMtHqjmg8yKeMLnpBpVN5ZuuY"
 
 const POCLockValue = 18446744073709551615
 
-var AssetsMap = map[string]int{"1-1": 8, "9-1": 8, "9-2": 18, "9-3": 6, "9-5": 6, "9-6": 18, "9-7": 18, "1-146": 18, "9-137": 18, "9-195": 18, "9-196": 18, "9-70": 8, "9-30": 18, "9-25": 18, "9-55": 18, "9-87": 18}
+var AssetsMap = map[string]int{"1-1": 8, "9-1": 8, "9-2": 18, "9-3": 6, "9-5": 6, "9-6": 18, "9-7": 18, "1-146": 18, "9-137": 18, "9-195": 18, "9-196": 18, "9-70": 8, "9-30": 18, "9-25": 18, "9-55": 18, "9-87": 18, "9-232": 18}
